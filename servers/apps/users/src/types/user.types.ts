97f85ae8b422a2083e50ca411d88e20bf3a802d7
Amira Allagui <allagui_fawzi@outlook.fr>
2023-11-25 13:09:02 +0100

    business logic for users.service.ts : register ,login , getUsers

--- servers/apps/users/src/types/user.types.ts
+++ servers/apps/users/src/types/user.types.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { ObjectType, Field } from '@nestjs/graphql';
 import { User } from '../entities/user.entity';
 
--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto, RegisterDto } from './dto/user.dto';
+import { Response } from 'express';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService,
+    // private readonly prisma: JwtService
+    private readonly configService: ConfigService,
+  ) {}
+
+  //Register user service
+  async register(registerDto: RegisterDto, response: Response) {
+    const { name, email, password } = registerDto;
+    const user = { name, email, password };
+    return user;
+  }
+
+  //Login  service
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = { email, password };
+    return user;
+  }
+  //get all users
+  async getUsers() {
+    const users = [];
+    return users;
   }
 }
