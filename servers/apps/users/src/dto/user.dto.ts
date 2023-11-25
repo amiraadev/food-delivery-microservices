e0f1dd523f613ef1e508dcc3c1c94fe3baba3ad6
Amira Allagui <allagui_fawzi@outlook.fr>
2023-11-25 12:30:37 +0100

    user.dto.ts

---
+++ servers/apps/users/src/dto/user.dto.ts
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+@InputType()
+export class RegisterDto {
+  @Field()
+  @IsNotEmpty({ message: 'Name is required.' })
+  @IsString({ message: 'Name need to be one string.' })
+  name: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'password is required.' })
+  @MinLength(8, { message: 'Password must be at least 8 characters' })
+  password: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Email is required.' })
+  @IsEmail({}, { message: 'Email is invalid' })
+  email: string;
+}
+@InputType()
+export class LoginDto {
+  @Field()
+  @IsNotEmpty({ message: 'Name is required.' })
+  @IsString({ message: 'Name need to be one string.' })
+  name: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'password is required.' })
+  @MinLength(8, { message: 'Password must be at least 8 characters' })
+  password: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Email is required.' })
+  @IsEmail({}, { message: 'Email is invalid' })
+  email: string;
+}
